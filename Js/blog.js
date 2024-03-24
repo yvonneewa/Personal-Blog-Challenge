c68779b0be475daa371df8d7b9d081e004234ef8
Yvonne <yowantings@gmail.com>
2024-03-24 10:47:16 -0600

    added code to the html

---
+++ Js/blog.js
@@ -0,0 +1,36 @@
+//display blog posts
+// blog title 
+// blog content
+// posted by
+// take info from form and put it into local storage
+// create 4  elements- one for each blog post
+//add element - text.content
+//apenend the element
+// hardcode result to blog.html - (use css)put class with the elements in html to style it - go to javascript and add that class for every object
+//hard code first -then come back to the array-create - the elememts just like you had it hardcoded-- take the hard code out- run the javascript- and your good 
+//when i take out the hard code am i taking out the hard html div elemets- yes because thats what your going to create when you create an element - keep a copy of  your hard code becuase your gonna want to reference it - basically hard code so you know its gonna look good, you know what kind of classes and elements you need- than try and figure out how to recreate it in javascript so you dont need any html
+//all your html should be everything thats already on there plus light and dark mode
+
+
+mock
+const blogs=[
+{
+    title: "Blog Title1"
+    content: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore atque iste voluptate repudiandae fugit nobis sit. Rerum, dignissimos ut quo voluptatum, eum accusantium debitis, quas at ex iste repellat rem.
+
+},
+{
+    title: "Blog Title2"
+    content: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore atque iste voluptate repudiandae fugit nobis sit. Rerum, dignissimos ut quo voluptatum, eum accusantium debitis, quas at ex iste repellat rem."
+    posted by: "name"
+},
+    title: "Blog Title3"
+    content: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore atque iste voluptate repudiandae fugit nobis sit. Rerum, dignissimos ut quo voluptatum, eum accusantium debitis, quas at ex iste repellat rem."
+    posted by: "name"
+{
+   title: "Blog Title4"
+    content: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore atque iste voluptate repudiandae fugit nobis sit. Rerum, dignissimos ut quo voluptatum, eum accusantium debitis, quas at ex iste repellat rem."
+    posted by: "name"
+},
+]
+
