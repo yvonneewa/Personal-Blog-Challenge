8a5f19cdeefb5743621fb8f2e59eaa55e211d62c
Yvonne <yowantings@gmail.com>
2024-03-24 18:18:09 -0600

    added css code

--- Js/blog.js
+++ Js/blog.js
@@ -1,5 +1,5 @@
 //display blog posts
-// blog title 
+// blog title
 // blog content
 // posted by
 // take info from form and put it into local storage
@@ -7,30 +7,41 @@
 //add element - text.content
 //apenend the element
 // hardcode result to blog.html - (use css)put class with the elements in html to style it - go to javascript and add that class for every object
-//hard code first -then come back to the array-create - the elememts just like you had it hardcoded-- take the hard code out- run the javascript- and your good 
+//hard code first -then come back to the array-create - the elememts just like you had it hardcoded-- take the hard code out- run the javascript- and your good
 //when i take out the hard code am i taking out the hard html div elemets- yes because thats what your going to create when you create an element - keep a copy of  your hard code becuase your gonna want to reference it - basically hard code so you know its gonna look good, you know what kind of classes and elements you need- than try and figure out how to recreate it in javascript so you dont need any html
 //all your html should be everything thats already on there plus light and dark mode
 
 
-mock
-const blogs=[
-{
-    title: "Blog Title1"
-    content: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore atque iste voluptate repudiandae fugit nobis sit. Rerum, dignissimos ut quo voluptatum, eum accusantium debitis, quas at ex iste repellat rem.
 
-},
-{
-    title: "Blog Title2"
-    content: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore atque iste voluptate repudiandae fugit nobis sit. Rerum, dignissimos ut quo voluptatum, eum accusantium debitis, quas at ex iste repellat rem."
-    posted by: "name"
-},
-    title: "Blog Title3"
-    content: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore atque iste voluptate repudiandae fugit nobis sit. Rerum, dignissimos ut quo voluptatum, eum accusantium debitis, quas at ex iste repellat rem."
-    posted by: "name"
-{
-   title: "Blog Title4"
-    content: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore atque iste voluptate repudiandae fugit nobis sit. Rerum, dignissimos ut quo voluptatum, eum accusantium debitis, quas at ex iste repellat rem."
-    posted by: "name"
-},
-]
+let switch = document.querySelector('#switch');
+let blogList = document.querySelector('#blogList');
+let title = document.querySelector('#title');
+let submit= document.querySelector('#dsubmit'); 
+
+document.getElementById("darkMode").onclick = function(){
+    darkMode.addEventListener('click', function() { 
+        console.log('Button was clicked'); 
+      }); 
+  };
+
+
+  document.getElementById("LightMode").onclick = function(){
+    lightMode.addEventListener('click', function() { 
+        console.log('Button was clicked'); 
+      }); 
+  };
+
+
+
+submit.getElementById("submit").onclick = function(){
+    submit.addEventListener('click', function() { 
+        console.log('Button was clicked'); 
+      }); 
+  };
+
+
+
+
+const userArray = [title,]
+localStorage.setItem('user', JSON.stringify(userArray));
 
